836fa1a507c4424a68bb1bd95f069a77e9e57ef4
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-21 20:34:07 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,39 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-public class Program{
+public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-       ChessMatch chessMatch = new ChessMatch();
+        ChessMatch chessMatch = new ChessMatch();
 
-       while(true){
-           UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(scan);
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(scan);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(scan);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(scan);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-       }
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                scan.nextLine();
+                
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                scan.nextLine();
+            }
+        }
     }
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -30,14 +30,19 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    public static ChessPosition readChessPosition(Scanner scan){
-        try {
-        String s = scan.nextLine();
-        char column = s.charAt(0);
-        int row = Integer.parseInt(s.substring(1));
-        return new ChessPosition(column, row);
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
-        catch(RuntimeException e ){
+
+    public static ChessPosition readChessPosition(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h2. ");
         }
     }
@@ -54,17 +59,15 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece) {
-    	if (piece == null) {
+        if (piece == null) {
             System.out.print("-");
-        }
-        else {
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
-	}
+    }
 }
