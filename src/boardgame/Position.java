9c8a6e9a3b53e3cac6de2280a499dcf8c71c5359
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-22 20:14:03 -0300

    Possible moves of a  piece

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -8,22 +8,28 @@ public class Position {
         this.row = row;
         this.column = column;
     }
-    
-    public int getRow(){
+
+    public int getRow() {
         return row;
     }
 
-    public void setRow(int row){
+    public void setRow(int row) {
         this.row = row;
     }
 
-    public int getColumn(){
+    public int getColumn() {
         return column;
     }
 
-    public void setColumn(int column){
-        this.column= column;
+    public void setColumn(int column) {
+        this.column = column;
     }
+
+    public void setValues(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
     @Override
     public String toString(){
         return row + ", "+ column;
