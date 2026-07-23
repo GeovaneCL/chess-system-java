b5a2d4e7d7cb52db851870d9a7eeb092fc32816e
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-22 22:57:27 -0300

    Checkmate logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,7 +17,7 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
     
-    public ChessPosition geChessPosition(){
+    public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
     }
 
